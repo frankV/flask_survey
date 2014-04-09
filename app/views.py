18654b07343dff6a3273a52d5afb3d2d93cf1de7
k <rkk09c@gmail.com>
2014-04-09 17:13:25 -0400

    User logins are now functional

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from models import User, ROLE_USER, ROLE_ADMIN
 def load_user(id):
 	return User.query.get(int(id))
 
-@app.route('/create_acct' , methods=['GET','POST'])
+@app.route('/create_acct/' , methods=['GET','POST'])
 def create_acct():
 	form = RegistrationForm(request.form)
 	if form.validate_on_submit():
@@ -21,7 +21,7 @@ def create_acct():
 		return redirect(url_for('index'))
 	return render_template('create_acct.html', title = "Create Account", form=form)
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login/',methods=['GET','POST'])
 def login():
 	form = LoginForm(request.form)
 	if form.validate_on_submit():
@@ -31,21 +31,10 @@ def login():
 		return redirect(request.args.get("next") or url_for("index"))
 	return render_template('login.html', title = "Login", form=form)
 
-    # if request.method == 'GET':
-    #     return render_template('login.html')
-    # username = request.form['username']
-    # passwd = request.form['passwd']
-    # registered_user = User.query.filter_by(username=username,passwd=passwd).first()
-    # if registered_user is None:
-    #     flash('Username or Password is invalid' , 'error')
-    #     #add attempt to a log organized by time
-    #     #userid passwd event time  event = failed, success, retrieve passwd
-    #     #retrieve passwd as event
-    #     #login attempts, failed
-    #     return redirect(url_for('login'))
-    # login_user(registered_user)
-    # flash('Logged in successfully')
-    # return redirect(request.args.get('next') or url_for('index'))
+@app.route('/forgot_passwd')
+def forgot_passwd():
+	user = g.user
+	return render_template ("forgot_passwd.html", title="Forgot Password")
 
 @app.route('/')
 @app.route('/index')
@@ -64,42 +53,6 @@ def about():
 @lm.user_loader
 def load_user(id):
 	return User.query.get(int(id))
-
-# @app.route('/login', methods = ['GET', 'POST'])
-# @oid.loginhandler
-# def login():
-# 	if g.user is not None and g.user.is_authenticated():		#if g is not anonymous
-# 		return redirect(url_for('index'))
-# 	form = LoginForm()
-# 	if form.validate_on_submit():
-# 		flash('Login requested for OpenID="' + form.openid.data + '", remember_me=' + str(form.remember_me.data))
-# 		session['remember_me'] = form.remember_me.data
-# 		return oid.try_login(form.openid.data, ask_for = ['username', 'email'])
-# 	return render_template("login.html",
-# 		title = "Login",
-# 		form = form,
-# 		providers = app.config['OPENID_PROVIDERS'])
-
-# @oid.after_login
-# def after_login(resp):		#resp = response from openid
-# 	if resp.email is None or resp.email == "":
-# 		flash('Invalid login. Please try again.')
-# 		return redirect(url_for('login'))
-# 	user = User.query.filter_by(email = resp.email).first()
-	
-# 	if user is None:
-# 		username = resp.username
-# 		if username is None or username == "":
-# 			username = resp.email.split('@')[0]
-# 		user = User(username = username, email = resp.email, role = ROLE_USER)
-# 		db.session.add(user)
-# 		db.session.commit()
-# 	remember_me = False
-# 	if 'remember_me' in session:
-# 		remember_me = session['remember_me']
-# 		session.pop('remember_me', None)
-# 	login_user(user, remember = remember_me)
-# 	return redirect(request.args.get('next') or url_for('index'))
 
 @app.before_request
 def before_request():
