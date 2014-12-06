06e25a13688ec4132d8b3ce0094d6d119d095d0d
Frank Valcarcel <fvalcarcel@me.com>
2014-12-06 16:52:11 -0500

    merging, few template fixes

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,11 @@ from flask import Flask, session
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
+
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-# assets = Environment(app)
+
 
 lm = LoginManager()
 lm.init_app(app)
