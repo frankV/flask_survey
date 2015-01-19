cb0d5ba7dd72c1c962a374d953318d73947033b3
Frank Valcarcel <fvalcarcel@me.com>
2015-01-19 01:29:47 -0500

    updated column headers for Survey 3 and 4 so that they make sense, how about that? also added missing columns to Survey 3 and 4, pure laziness is why they were missing

--- app/forms.py
+++ app/forms.py
@@ -3,12 +3,14 @@ from wtforms import widgets
 from models import User
 from app import db
 
+
 def validate_login(form, field):
     user = form.get_user()
     if user is None:
         raise validators.ValidationError('Invalid user')
     if user.password != form.password.data:
         raise validators.ValidationError('Invalid password')
+
 
 class LoginForm(Form):
     email = fields.TextField(validators=[Required(), Email()])
@@ -17,11 +19,13 @@ class LoginForm(Form):
     def get_user(self):
         return db.session.query(User).filter_by(email=self.email.data).first()
 
+
 class ForgotPasswordForm(Form):
     email = fields.TextField(validators=[Required(), Email()])
 
     def get_user(self):
         return db.session.query(User).filter_by(email=self.email.data).first()
+
 
 class RegistrationForm(Form):
     email = fields.TextField('Email Address', validators=[Required(), Email(), Regexp('[^@]+@[^@]+[fsu]+\.[edu]+')])
@@ -36,12 +40,14 @@ class RegistrationForm(Form):
         if db.session.query(User).filter_by(email=self.email.data).count() > 0:
             raise validators.ValidationError('Duplicate email')
 
+
 class NewPass(Form):
     password = fields.PasswordField('New Password', [
         validators.Required(), validators.Length(min=8, max=20),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = fields.PasswordField(validators=[Required()])
+
 
 class Survey1Form(Form):
     gender = fields.RadioField('What is your gender?', choices=[('M', 'Male'), ('F', 'Female'), ('O', 'I prefer not to answer')], validators = [Required()], default=None)
@@ -51,6 +57,7 @@ class Survey1Form(Form):
         ('Scnd', 'Some college, no degree'), ('Assoc', 'Associates Degree'), ('Bach', 'Bachelors degree'), ('Grad', 'Graduate degree (Masters, Doctorate, etc.)'), ('O', 'Other')],
         validators=[Required()])
     language = fields.TextField('What is your native language', validators=[Required()])
+
 
 class Survey2Form(Form):
     major = fields.RadioField('Are you majoring in or do you have a degree or job in computer science, computer engineering, information technology, or a related field?',
@@ -62,6 +69,7 @@ class Survey2Form(Form):
         ('N', 'No, I use my old passwords that I have already created for my other accounts'),
         ('O', 'I mostly create a new password, but sometimes use old passwords')], validators=[Required()])
     department = fields.TextField('In what department are you majoring', validators=[Required()])
+
 
 class Survey3Form(Form):
     choose_names = fields.BooleanField('Names of family members, relatives, close friends')
@@ -103,6 +111,7 @@ class Survey3Form(Form):
     addedwords=fields.BooleanField('Added Words or Letters')
     deletedwords=fields.BooleanField('Deleted Words or Letters')
 
+
 class Survey4Form(Form):
     computerTime = fields.RadioField('How long have you been using a computer?', choices=[
         ('0-2', '0 to 2 Years'), ('3-5', '3 to 5 Years'), ('6-10', '6 to 10 Years'), ('mt10', 'More than 10 years')], validators=[Required()], default=None)
@@ -122,42 +131,3 @@ class Survey4Form(Form):
     how_write_down = fields.BooleanField('I write down my password on a piece of paper')
     how_no = fields.BooleanField('No, I do not save my passwords. I remember them.')
     comments = fields.TextAreaField('If you have any additional feedback about passwords or this survey, please enter your comments here.', default=None)
-
-# choose = fields.SelectMultipleField('How did you choose your new password? Were you influenced by any of the following? (Please check all that apply.)',
-    #     choices = [('names', 'Names of family members, relatives, close friends'), ('numbers', 'Familiar numbers (birth date, telephone number, street address, employee number, etc.)'),
-    #     ('songs', 'Songs, movies, television shows, books, poetry or games'), ('mnemonic', 'Scientific or other educational mnemonics'),
-    #     ('sports', 'Sports teams and players'), ('famous', 'Names of famous people or characters'), ('words', 'Words in a language other than English')],
-    #     default=None, option_widget=widgets.CheckboxInput(),
-    #     widget=widgets.ListWidget(prefix_label=False))
-# secure = fields.SelectMultipleField('When creating your new password, did you consider any of the following policies to make your password more secure? (Please check all that apply.)',
-    #     choices = [('numbers', 'Include numbers'), ('upper_case', 'Include upper case letters'), ('symbols', 'Include symbols'),
-    #     ('eight_chars', 'Have 8 or more characters'), ('no_dict', 'Not contain dictionary words'),
-    #     ('adjacent', 'Not containing a sequence of adjacent or repeated characters on your keyboard (e.g. qwerty)'),
-    #     ('nothing', 'I did not consider any policy')],
-    #     default=None, option_widget=widgets.CheckboxInput(),
-    #     widget=widgets.ListWidget(prefix_label=False))
-# numberPart = fields.SelectMultipleField('If you created your new password based on one of your old passwords, did you consider changing the number part in one of the following ways?',
-    #     choices=[('N', 'Not applicable'), ('added_digits', 'Added digits'), ('deleted_digits', 'Deleted digits'), ('substituted_digits', 'Substituted digits'),
-    #     ('O', 'Other')], validators=[Required()],
-    #    default=None, option_widget=widgets.CheckboxInput(),
-    #     widget=widgets.ListWidget(prefix_label=False))
-    # charPart = fields.SelectMultipleField('If you created your new password based on one of your old passwords, did you consider changing the special character part in one of the following ways?',
-    #     choices=[('N', 'Not applicable'), ('added_symbols', 'Added symbols'), ('deleted_symbols', 'Deleted symbols'), ('substituted_symbols', 'Substituted symbols'),
-    #     ('O', 'Other')], validators=[Required()],
-    #     default=None, option_widget=widgets.CheckboxInput(),
-    #     widget=widgets.ListWidget(prefix_label=False))
-# passwordCreation = fields.SelectMultipleField('How do you usually create passwords for your accounts? (Please check all that apply.)', choices=[
-    #     ('random', 'Randomly generate a password using special software or apps'), ('reuse', 'Reuse a password that is used for another account'),
-    #     ('modify', 'Modify a password that is used for another account'), ('new', 'Create a new password using a familiar number or a name of a family member'),
-    #     ('substitute', 'Choose a word and substitute some letters with numbers of symbols (for example @ for a)'),
-    #     ('multiword', 'Use a pass-phrase consisting of several words'), ('phrase', 'Choose a phrase and use the first letters of each word'),('O', 'Other')],
-    #     validators=[Required()],
-    #     default=None, option_widget=widgets.CheckboxInput(),
-    #     widget=widgets.ListWidget(prefix_label=False))
-    # howStored = fields.SelectMultipleField('Do you store your passwords? If yes how? (Check all that apply.)',
-    #     choices=[('regular_file ', 'I store my passwords in a regular file / document on my computer.'),
-    #     ('encrypted', 'I store my passwords in an encrypted computer file'), ('software', 'I use password management software to securely store my passwords'),
-    #     ('cellphone', 'I store my passwords on my cellphone / smartphone'), ('browser', 'I save my passwords in the browser'),
-    #     ('write_down', 'I write down my password on a piece of paper'), ('no', 'No, I do not save my passwords. I remember them.')],
-    #     default=None, option_widget=widgets.CheckboxInput(),
-    #     widget=widgets.ListWidget(prefix_label=False))
